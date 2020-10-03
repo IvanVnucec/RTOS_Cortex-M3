8e341828484fa67cf6caecce59a8d249d1b5be91
IvanVnucec <vnucec.ivan@gmail.com>
2020-10-03 12:26:03 +0200

    mutex timeout and error handling
    Implemented mutex timeout and error handling.

--- source/Os/mutex.h
+++ source/Os/mutex.h
@@ -31,6 +31,14 @@ struct OS_Mutex_STRUCT {
 };
 
 
+enum OS_MutexError_STRUCT {
+	OS_MUTEX_ERROR_NONE,
+	OS_MUTEX_ERROR_NULL_PTR,
+	OS_MUTEX_ERROR_TIMEOUT,
+	OS_MUTEX_ERROR_NOT_OWNER_POST
+};
+
+
 /*******************************************************************************************************
  *                         GLOBAL AND STATIC VARIABLES
  ******************************************************************************************************/
@@ -40,9 +48,9 @@ extern OS_TCB_S *OS_TCBCurrent;
 /*******************************************************************************************************
  *                         PUBLIC FUNCTION PROTOTYPES
  ******************************************************************************************************/
-void OS_MutexInit(OS_Mutex_S *mutex);
-void OS_MutexPend(OS_Mutex_S *mutex, OS_Error_E *err);
-void OS_MutexPost(OS_Mutex_S *mutex, OS_Error_E *err);
+void OS_MutexInit(OS_Mutex_S *mutex, OS_MutexError_E *err);
+void OS_MutexPend(OS_Mutex_S *mutex, uint32_t timeout, OS_MutexError_E *err);
+void OS_MutexPost(OS_Mutex_S *mutex, OS_MutexError_E *err);
 
 
 #endif /* #ifndef MUTEX_H */
