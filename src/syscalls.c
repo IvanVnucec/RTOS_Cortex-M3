51fbf51b77b639945003f504c8cd8126b9f2e546
IvanVnucec <vnucec.ivan@gmail.com>
2021-02-15 21:30:12 +0100

    added QEMU exit with exit() function

--- src/syscalls.c
+++ src/syscalls.c
@@ -1,8 +1,9 @@
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
 
 // LIBC SYSCALLS
 /////////////////////
@@ -14,67 +15,77 @@ extern uint32_t UART0_getc(void);
 extern int end;
 
 caddr_t _sbrk(int incr) {
-  static unsigned char *heap = NULL;
-  unsigned char *prev_heap;
+    static unsigned char *heap = NULL;
+    unsigned char *prev_heap;
 
-  if (heap == NULL) {
-    heap = (unsigned char *)&end;
-  }
-  prev_heap = heap;
+    if (heap == NULL) {
+        heap = (unsigned char *)&end;
+    }
+    prev_heap = heap;
 
-  heap += incr;
+    heap += incr;
 
-  return (caddr_t) prev_heap;
+    return (caddr_t)prev_heap;
 }
 
 int _close(int file) {
-  return -1;
+    return -1;
 }
 
 int _fstat(int file, struct stat *st) {
-  st->st_mode = S_IFCHR;
+    st->st_mode = S_IFCHR;
 
-  return 0;
+    return 0;
 }
 
 int _isatty(int file) {
-  return 1;
+    return 1;
 }
 
 int _lseek(int file, int ptr, int dir) {
-  return 0;
+    return 0;
 }
 
 void _exit(int status) {
-  __asm("BKPT #0");
-  while (1) {}
+    #ifdef QEMU_ENABLED
+    printf("%d\n", status);
+
+    register uint32_t r0 __asm__("r0") = 0x18;
+    register uint32_t r1 __asm__("r1") = 0x20026;
+
+    __asm__ volatile("bkpt #0xAB");
+
+    (void)r0;
+    (void)r1;
+    #endif
+
+    while(1);
 }
 
 void _kill(int pid, int sig) {
-  return;
+    return;
 }
 
 int _getpid(void) {
-  return -1;
+    return -1;
 }
 
 /* in order to print with printf functions on QEMU serial port
 printf string must end with '\n' character.
 */
-int _write(int file, char *ptr, int len)
-{
+int _write(int file, char *ptr, int len) {
     int i;
 
     if (file == STDOUT_FILENO || file == STDERR_FILENO) {
         for (i = 0; i < len; i++) {
             if (ptr[i] == '\n') {
-                #ifdef QEMU_ENABLED
+#ifdef QEMU_ENABLED
                 UART0_putc('\r');
-                #endif
+#endif
             }
-            #ifdef QEMU_ENABLED
+#ifdef QEMU_ENABLED
             UART0_putc(ptr[i]);
-            #endif
+#endif
         }
         return i;
     }
@@ -82,8 +93,6 @@ int _write(int file, char *ptr, int len)
     return -1;
 }
 
-int _read (int file, char * ptr, int len) {
-  return -1;
+int _read(int file, char *ptr, int len) {
+    return -1;
 }
-
-
