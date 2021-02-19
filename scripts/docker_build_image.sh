0393562fc87145c5c36a093455be4a8908b25c60
IvanVnucec <vnucec.ivan@gmail.com>
2021-02-19 23:32:54 +0100

    reformatting

--- scripts/docker_build_image.sh
+++ scripts/docker_build_image.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 # load constants
 . ./scripts/constants.env
 
+# should be eqal to the docker_run_container.sh
+
+
 # build and run Docker image
 echo "Building Docker containter."
-docker build -t="$CONTAINER_NAME" .
+docker build -t="$IMAGE_NAME" .
 exit $?
--- scripts/docker_run_container.sh
+++ scripts/docker_run_container.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 # load constants
 . ./scripts/constants.env
 
+readonly HOST_WORKDIR=/app  # defined in Dockerfile
+
 echo "Running Docker container."
-docker run --rm -v "$(pwd):/app" $CONTAINER_NAME
+docker run --rm -v "$(pwd):$HOST_WORKDIR" $IMAGE_NAME
 exit $?
