697d47d69b0dd6f79d2132ff18258df1a309c0b7
IvanVnucec <vnucec.ivan@gmail.com>
2021-02-20 01:36:29 +0100

    reformated check tests script

--- scripts/check_tests.sh
+++ scripts/check_tests.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-retval="$( tail -n 1 logs/stdout.log )"
-retval=${retval::1}
+# load constants
+. ./scripts/constants.env
 
-if [ ${retval::1} = 0 ]; then
-    echo "Test PASS."
-    exit 0
+if [ -d "$LOGS_DIR" ]; then
+    retval="$( tail -n 1 $LOGS_DIR/stdout.log )"
+    retval=${retval::1}
+
+    if [ ${retval::1} = 0 ]; then
+        echo "Test PASS."
+        exit 0
+    else
+        echo "Test FAILED."
+        exit 1
+    fi
+
 else
-    echo "Test FAILED."
-    exit 1
+    echo "ERROR: No $LOGS_DIR folder."
+    exit 2
 fi
